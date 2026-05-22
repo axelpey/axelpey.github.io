776b4b446b2b63b3281641e216b346ad2904242a
Axel Peytavin <peytavin@stanford.edu>
2026-05-21 23:55:32 -0700

    Add Code Nostalgia blog post

--- app/blog/posts.ts
+++ app/blog/posts.ts
@@ -7,6 +7,40 @@ export interface BlogPost {
 
 export const posts: BlogPost[] = [
   {
+    slug: "code-nostalgia",
+    title: "Code Nostalgia",
+    date: "2026-05-21",
+    content: `When I was a kid, I used to find solace coming in my room and coding. I would look at my screen, which would be the only light in my dark room, put my hands on the keyboard, and lock in.
+I would enter another world, one of digital wonders. The internet seemed vast and diverse, with multiple different corners. I would hang out in my corner and felt like for the rest of history, the digital spaces would continue growing and diversifying.
+
+And that I would be able to make my own, and just, code and build.
+
+For many, coding started as a way to make something because they saw others getting money like that, or because of classes.
+That was not my case, I liked it because it was my escape. I became so good at it that it carried me throughout all my studies.
+
+The reason I liked math was because it helped me code better. I liked physics because I could simulate it with code.
+
+I came to Silicon Valley because of code.
+
+And now, it’s going away.
+In the meantime I grew and lived many things and diversified my interests. I also learned at some point to like maths more than code, and during my years at Stanford I did prefer white boards to black screens. But still, I would sometimes sit and feel how talented I was at code. It was reassuring in a way.
+
+Code going away is just a fact of life, of history.
+In the grand scheme of things, we only had code for what… 75 years?
+For the first 25, it was gate kept in big companies, then the PC arrived and we all could suddenly code and build.
+
+For the next 50 years, careers, companies, personal success stories were based on code.
+Software spread everywhere, ate the world while lifting a generation. Numerous people, like me as a kid, fell in love with it.
+
+But now it’s time to say goodbye. Some will say there’s nothing wrong with that, as human beings we need to learn to adapt, we always did, and we always learned to love the new things we learned.
+But we will always look back to this particular chapter of history, this beautiful world made of code that we had all created for ourselves. It was maybe the greatest collaboration at humanity’s scale, and it is now closing.
+
+No longer will we have late nights debugging a SIGSEGV or an environment issue while browsing reddit here and there.
+We will leave the maintenance of this digital world to our new AI partners, only coming back from time to time to observe the World Wide Web, full of robots.
+
+A place that we built, but that does not belong to us anymore.`,
+  },
+  {
     slug: "personal-music",
     title: "Personal music",
     date: "2026-05-12",
